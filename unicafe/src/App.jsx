cfd27e04cf8f29d881b1d2764cfe9a750c7ab055
Saurabh Kamboj <skamboj97@gmail.com>
2025-08-17 16:23:08 +0530

    Modify file

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -6,8 +6,9 @@ const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const Stat = ({ type, value }) => <tr><td>{type}</td><td>{value}</td></tr>
 
-const Stats = ({ good, neutral, bad, average}) => {
+const Stats = ({ good, neutral, bad }) => {
   let total = good + neutral + bad
+  let average = good - bad
 
   if (total === 0) {
     return (
@@ -33,20 +34,17 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [average, setAverage] = useState(0)
 
   const handleOnClick = (type) => {
     switch (type) {
       case 'good':
         setGood(good + 1)
-        setAverage(average + 1)
         break
       case 'neutral':
         setNeutral(neutral + 1)
         break
       case 'bad':
         setBad(bad + 1)
-        setAverage(average - 1)
         break
     }
   }
@@ -59,7 +57,7 @@ const App = () => {
       <Button onClick={() => handleOnClick('bad')} text='bad' />
 
       <Header text='statistics' />
-      <Stats good={good} neutral={neutral} bad={bad} average={average} />
+      <Stats good={good} neutral={neutral} bad={bad} />
     </>
   )
 }
