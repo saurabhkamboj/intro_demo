e90bb024dcbdd2f478915921d7a16b9e16386196
Saurabh Kamboj <skamboj97@gmail.com>
2025-08-17 15:00:39 +0530

    Modify file

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,31 +1,103 @@
-const Hello = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old.</p>
-    </div>
-  )
-}
+import { useState } from 'react'
 
-const Footer = () => {
-  return (
-    <div>
-      greeting app created by <a href='https://github.com/saurabhkamboj'>Saurabh</a>
-    </div>
-  )
-}
+/*
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-  const name = 'Peter'
-  const age = 30
-  const friends = ['Joe', 'John']
+  const [ counter, setCounter ] = useState(0)
+  console.log('rendering with counter value: ', counter)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
+  const decreaseByOne = () => setCounter(counter - 1)
+
   return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name={name} age={age} />
-      <div><p>{friends.join(', ')}</p></div>
-      <Footer />
-    </>
+   <div>
+    <Display counter={counter} />
+    <Button onClick={increaseByOne} text='plus' />
+    <Button onClick={setToZero} text='reset' />
+    <Button onClick={decreaseByOne} text='minus' />
+   </div>
+  )
+}
+*/
+
+/*
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  // const [total, setTotal] = useState(0)
+
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    // setTotal(updatedLeft + right)
+  }
+
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    // setTotal(left + updatedRight)
+  }
+
+  return (
+    <div>
+      {left}
+      <Button onClick={handleLeftClick} text='left' />
+      <Button onClick={handleRightClick} text='right' />
+      {right}
+
+      <History allClicks={allClicks} />
+    </div>
+  )
+}
+*/
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const Display = ({ value }) => <div>{value}</div>
+
+const App = () => {
+  const [value, setValue] = useState(10)
+  
+
+  const setToValue = (newValue) => () => {
+    console.log('value now', newValue)  // print the new value to console
+    setValue(newValue)
+  }
+  
+  return (
+    <div>
+      <Display value={value} />
+
+      <Button onClick={setToValue(1000)} text='thousand' />
+      <Button onClick={setToValue(0)} text='reset' />
+      <Button onClick={setToValue(value + 1)} text='increment' />
+    </div>
   )
 }
 
