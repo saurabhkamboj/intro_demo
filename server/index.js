056926c078e961036fc89504b1ef5a37d130a641
Saurabh Kamboj <skamboj97@gmail.com>
2025-08-22 06:51:32 +0530

    Modify folder

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 app.use(express.static('dist'))
