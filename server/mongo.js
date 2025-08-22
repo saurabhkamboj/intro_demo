127a3757f21daf271a8ee58f3e4282e2896b692d
Saurabh Kamboj <skamboj97@gmail.com>
2025-08-22 11:55:21 +0530

    Modify folder

---
+++ server/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://saurabh:${password}@cluster0.cqipty2.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note1 = new Note({
+  content: 'HTML is easy',
+  important: true,
+})
+
+const note2 = new Note({
+  content: 'Browser can execute only JavaScript',
+  important: false,
+})
+
+const note3 = new Note({
+  content: 'GET and POST are the most important methods of HTTP protocol',
+  important: true,
+})
+
+note1.save()
+note2.save()
+note3.save().then(() => {
+  mongoose.connection.close()
+})
