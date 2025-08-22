5bb4a895e082a635f3a85f70f7fb4d2f0a74b7df
Saurabh Kamboj <skamboj97@gmail.com>
2025-08-22 15:16:15 +0530

    Add validation for adding notes

--- server/index.js
+++ server/index.js
@@ -20,6 +20,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -51,7 +53,7 @@ app.get('/api/notes/:id', (request, response, next) => {
   .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (!body.content) {
@@ -65,9 +67,11 @@ app.post('/api/notes', (request, response) => {
     important: body.important || false,
   })
 
-  note.save().then((savedNote) => {
+  note.save()
+  .then((savedNote) => {
     response.json(savedNote)
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
